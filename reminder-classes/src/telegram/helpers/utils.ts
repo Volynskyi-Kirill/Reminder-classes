0ad6bb0fc73d6f640d8dd569b2d562512dac0d2f
Volynskyi-Kirill <kirill108bol@gmail.com>
2024-09-04 17:37:25 +0200

    можно смотреть рассписание

--- reminder-classes/src/telegram/helpers/utils.ts
+++ reminder-classes/src/telegram/helpers/utils.ts
@@ -3,7 +3,9 @@ import {
   startOfWeek,
   addMinutes,
   format,
+  addDays,
 } from 'date-fns';
+import { CLASSES_SCHEDULE, SCHEDULE_COMMANDS, STUDY_DAYS } from './constants';
 
 export function isNowNumeratorWeek() {
   const firstNumeratorWeekStart = new Date(2024, 8, 2);
@@ -51,6 +53,51 @@ export function getCronExpressionForLesson(day: string, time: string): string {
   return cronExpression;
 }
 
-export function buildMessage(lessonName: string, link: string) {
+export function buildNotificationClassMessage(
+  lessonName: string,
+  link: string,
+) {
   return `Нагадування: ${lessonName} починається за 2 хвилини! \nПосилання: ${link}`;
+}
+
+export function buildScheduleAvailableCommands(getSchedulePattern: string) {
+  return Object.values(SCHEDULE_COMMANDS)
+    .map((day) => {
+      return `/${getSchedulePattern}_${day}`;
+    })
+    .join('\n');
+}
+
+function getDayForSchedule(day: string) {
+  const daysOfWeek = Object.values(STUDY_DAYS);
+  const today = new Date();
+
+  let date;
+  if (day === SCHEDULE_COMMANDS.TODAY) {
+    date = today;
+  } else if (day === SCHEDULE_COMMANDS.TOMORROW) {
+    date = addDays(today, 1);
+  } else {
+    return day;
+  }
+
+  const dayOfWeekIndex = (date.getDay() + 6) % 7;
+  return daysOfWeek[dayOfWeekIndex];
+}
+
+const timeColumnWidth = 20;
+const lessonColumnWidth = 40;
+
+export function getScheduleByDay(day: string) {
+  const resolvedDay = getDayForSchedule(day);
+  const schedule = CLASSES_SCHEDULE[resolvedDay];
+
+  return Object.entries(schedule)
+    .map(([time, lesson]) => {
+      const lessonName =
+        lesson && 'lessonName' in lesson ? lesson.lessonName : 'немає пари';
+
+      return `${time.padEnd(timeColumnWidth)} ${lessonName.padEnd(lessonColumnWidth)}`;
+    })
+    .join('\n');
 }
--- reminder-classes/src/telegram/polling.service.ts
+++ reminder-classes/src/telegram/polling.service.ts
@@ -1,23 +1,56 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as TelegramBot from 'node-telegram-bot-api';
-import { Message } from 'node-telegram-bot-api';
-
-// нужен ли вообще полинг, если бот просто должен слоть сообщения по таймингу? но пускай будет на будущие
+import { SCHEDULE_COMMANDS } from './helpers/constants';
+import {
+  buildScheduleAvailableCommands,
+  getScheduleByDay,
+} from './helpers/utils';
 
 @Injectable()
 export class PollingService {
   private bot;
+  private getSchedulePattern;
   private readonly studyChatId: number;
 
   constructor(private readonly configService: ConfigService) {
+    this.getSchedulePattern = this.configService.get('GET_SCHEDULE_PATTERN');
     this.studyChatId = Number(this.configService.get('STUDY_CHAT_ID'));
     const token = this.configService.get('TG_API_KEY');
     this.bot = new TelegramBot(token, { polling: true });
-    this.bot.on('message', (msg) => this.onMessage(msg));
+    this.registerCommands();
   }
 
-  async onMessage(msg: Message) {
-    const text = msg.text ?? msg.caption;
+  private registerCommands() {
+    Object.values(SCHEDULE_COMMANDS).forEach((day) => {
+      this.bot.onText(
+        new RegExp(`^\/${this.getSchedulePattern}_${day}$`, 'i'),
+        (msg) => {
+          this.sendScheduleForDay(msg.chat.id, day);
+        },
+      );
+    });
+
+    this.bot.onText(/\/schedule_commands/, (msg) =>
+      this.sendScheduleCommands(msg.chat.id),
+    );
+    this.bot.onText(/\/help/, (msg) => this.sendHelp(msg.chat.id));
+  }
+
+  private async sendScheduleForDay(chatId: number, day: string) {
+    const schedule = getScheduleByDay(day);
+    await this.bot.sendMessage(chatId, `Розклад на ${day}: \n\n${schedule}`);
+  }
+
+  private async sendScheduleCommands(chatId: number) {
+    const availableCommands = buildScheduleAvailableCommands(
+      this.getSchedulePattern,
+    );
+    await this.bot.sendMessage(chatId, availableCommands);
+  }
+
+  private async sendHelp(chatId: number) {
+    const availableCommands = '/schedule_commands';
+    await this.bot.sendMessage(chatId, availableCommands);
   }
 }
